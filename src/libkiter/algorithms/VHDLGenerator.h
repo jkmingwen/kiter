816184ed2ede622cdae58bed9580b366995d5640
Jaime <jkmingwen@gmail.com>
2021-06-21 15:58:28 +0800

    Add buffer component generation

--- src/libkiter/algorithms/VHDLGenerator.h
+++ src/libkiter/algorithms/VHDLGenerator.h
@@ -26,6 +26,7 @@ namespace algorithms {
   void generateOperator(VHDLComponent comp, std::string componentDirectory);
   void generateCircuit(VHDLCircuit circuit, std::string outputDirectory);
   std::string generateComponent(VHDLComponent comp, std::string circuitName);
+  void generateBuffer(std::string componentDirectory, std::string bufferRefFile);
   /* std::string printStatus(models::Dataflow* const dataflow); */
 }
 
