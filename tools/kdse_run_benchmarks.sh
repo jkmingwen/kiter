0c9e59cee9e1bb63f6cb1b3571a10061c352677b
Jaime <jkmingwen@gmail.com>
2021-11-22 18:40:52 +0800

    Update DSE test scripts

--- tools/kdse_run_benchmarks.sh
+++ tools/kdse_run_benchmarks.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # This script runs the benchmarks used in the paper on K-periodic driven DSE.
+# Tests for:
+# - K-periodic/critical cycle driven DSE (KDSE)
+# - Symbolic execution/storage dependency driven DSE (SDF3)
+# - Symbolic execution/storage dependency drive DSE using SCC (SYMB_EXEC)
+# - Periodic DSE (1-DSE)
 
 MAX_TIME=2m
 
 KITER="./Release/bin/kiter"
 SDF3="./tools/sdf3/sdf3_custom/sdf3/build/release/Linux/bin/sdf3analysis-sdf"
 CSDF3="./tools/sdf3/sdf3_custom/sdf3/build/release/Linux/bin/sdf3analysis-csdf"
+SDF3_LIB="./tools/sdf3/sdf3_custom/sdf3/build/release/Linux/lib"
 
 SDF3_BENCH_DIR="benchmarks/sdf3mem/"
 KITER_BENCH_DIR="benchmarks/IB5CSDF/"
@@ -39,27 +45,35 @@ fi
 
 
 LOGDIR=$1
-
+if [ ! -d ${LOGDIR} ]; then
+    echo -ne "Error directory ${LOGDIR} not found: create the directory and try again.\n"
+    exit 1
+fi
 
 COMMAND_PREFIX="timeout --foreground  ${MAX_TIME}"
 
 
 for graph in bipartite samplerate modem satellite fig8 h263decoder; do
     echo "Run ${graph}"
-    ${COMMAND_PREFIX} ${KITER} -f ${SDF3_BENCH_DIR}${graph}.xml -a KPeriodicThroughputwithDSE -p LOGDIR="${LOGDIR}"  > /dev/null 2> /dev/null
-    ${COMMAND_PREFIX} ${KITER} -f ${SDF3_BENCH_DIR}${graph}.xml -a PeriodicDSE -p LOGDIR="${LOGDIR}"  > /dev/null 2> /dev/null
-    ${COMMAND_PREFIX} ${KITER} -f ${SDF3_BENCH_DIR}${graph}.xml -a SPeriodicDSE -p LOGDIR="${LOGDIR}"  > /dev/null 2> /dev/null
-    SDF3LOGDIR="${LOGDIR}" ${COMMAND_PREFIX} ${SDF3} --graph ${SDF3_BENCH_DIR}${graph}.xml --algo buffersize  > /dev/null 2> /dev/null
+    ${COMMAND_PREFIX} ${KITER} -f ${SDF3_BENCH_DIR}${graph}.xml -a ThroughputBufferingDSE -p LOGDIR="${LOGDIR}/"  > /dev/null 2> /dev/null
+    ${COMMAND_PREFIX} ${KITER} -f ${SDF3_BENCH_DIR}${graph}.xml -a ThroughputBufferingDSE -p SYMB_EXEC_ORIGINAL=t -p LOGDIR="${LOGDIR}/"  > /dev/null 2> /dev/null
+    ${COMMAND_PREFIX} ${KITER} -f ${SDF3_BENCH_DIR}${graph}.xml -a ThroughputBufferingDSE -p SYMB_EXEC_CORRECTED=t -p LOGDIR="${LOGDIR}/"  > /dev/null 2> /dev/null
+    ${COMMAND_PREFIX} ${KITER} -f ${SDF3_BENCH_DIR}${graph}.xml -a ThroughputBufferingDSE -p LOGDIR="${LOGDIR}/"  > /dev/null 2> /dev/null
+    ${COMMAND_PREFIX} ${KITER} -f ${SDF3_BENCH_DIR}${graph}.xml -a PeriodicDSE -p LOGDIR="${LOGDIR}/"  > /dev/null 2> /dev/null
+    ${COMMAND_PREFIX} ${KITER} -f ${SDF3_BENCH_DIR}${graph}.xml -a SPeriodicDSE -p LOGDIR="${LOGDIR}/"  > /dev/null 2> /dev/null
+    SDF3LOGDIR="${LOGDIR}/" ${COMMAND_PREFIX} ${SDF3} --graph ${SDF3_BENCH_DIR}${graph}.xml --algo buffersize  > /dev/null 2> /dev/null
 done
 
 for graph in BlackScholes Echo PDectect H264 JPEG2000; do
     echo "Run ${graph}"
-    ${COMMAND_PREFIX} ${KITER} -f ${KITER_BENCH_DIR}${graph}.xml -a KPeriodicThroughputwithDSE -p LOGDIR="${LOGDIR}" > /dev/null 2> /dev/null
-    ${COMMAND_PREFIX} ${KITER} -f ${KITER_BENCH_DIR}${graph}.xml -a PeriodicDSE -p LOGDIR="${LOGDIR}"  > /dev/null 2> /dev/null
-    ${COMMAND_PREFIX} ${KITER} -f ${KITER_BENCH_DIR}${graph}.xml -a SPeriodicDSE -p LOGDIR="${LOGDIR}"  > /dev/null 2> /dev/null
-    SDF3LOGDIR="${LOGDIR}" ${COMMAND_PREFIX} ${CSDF3} --graph ${KITER_BENCH_DIR}${graph}.xml --algo buffersize > /dev/null 2> /dev/null
+    ${COMMAND_PREFIX} ${KITER} -f ${KITER_BENCH_DIR}${graph}.xml -a ThroughputBufferingDSE -p LOGDIR="${LOGDIR}/" > /dev/null 2> /dev/null
+    ${COMMAND_PREFIX} ${KITER} -f ${KITER_BENCH_DIR}${graph}.xml -a ThroughputBufferingDSE -p SYMB_EXEC_ORIGINAL=t -p LOGDIR="${LOGDIR}/" > /dev/null 2> /dev/null
+    ${COMMAND_PREFIX} ${KITER} -f ${KITER_BENCH_DIR}${graph}.xml -a ThroughputBufferingDSE -p SYMB_EXEC_CORRECTED=t LOGDIR="${LOGDIR}/" > /dev/null 2> /dev/null
+    ${COMMAND_PREFIX} ${KITER} -f ${KITER_BENCH_DIR}${graph}.xml -a PeriodicDSE -p LOGDIR="${LOGDIR}/"  > /dev/null 2> /dev/null
+    ${COMMAND_PREFIX} ${KITER} -f ${KITER_BENCH_DIR}${graph}.xml -a SPeriodicDSE -p LOGDIR="${LOGDIR}/"  > /dev/null 2> /dev/null
+    LD_LIBRARY_PATH="${SDF3_LIB}" SDF3LOGDIR="${LOGDIR}" ${COMMAND_PREFIX} ${CSDF3} --graph ${KITER_BENCH_DIR}${graph}.xml --algo buffersize > /dev/null 2> /dev/null # NOTE manually specify LD_LIBRARY_PATH as a workaround for a bug in linker
 done
 
 
-./tools/dsereader.py --logdir ./logs/dse_logs/ bipartite samplerate modem satellite fig8 h263dec --opareto sdf3bench_pareto.png --odse sdf3bench_dse.png
+./tools/dsereader.py --logdir ./"${LOGDIR}"/dse_logs/ bipartite samplerate modem satellite fig8 h263dec --opareto sdf3bench_pareto.png --odse sdf3bench_dse.png
 ./tools/dsereader.py --logdir ./logs/dse_logs/ Black-scholes echo MotionJPEG2000_CODEC_cad_V3 ViolaJones_Methode1 encoderH264 --opareto kiterbench_pareto.png --odse kiterbench_dse.png
