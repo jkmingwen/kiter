616dbc84719fbfd08e8dcb108435053c5489b1fa
jkmingwen <jkmingwen@gmail.com>
2022-01-05 16:53:01 +0800

    Implement cycle count algorithm

--- src/libkiter/algorithms/analysis/cycle_detection.h
+++ src/libkiter/algorithms/analysis/cycle_detection.h
@@ -10,6 +10,7 @@
 
 
 #include <commons/KiterRegistry.h>
+#include <algorithms/schedulings.h>
 
 namespace models {
 	class Dataflow;
@@ -20,6 +21,18 @@ namespace algorithms {
 bool has_selfloops         (models::Dataflow* const  dataflow);
 bool has_cycles            (models::Dataflow* const  dataflow);
 void cycle_detection       (models::Dataflow* const  dataflow, parameters_list_t);
+  void find_simple_cycles(models::Dataflow* const  dataflow, parameters_list_t);
+  ARRAY_INDEX find_smallest_index(models::Dataflow* const dataflow,
+                                  std::map<int, std::vector<ARRAY_INDEX>> scc_map);
+  models::Dataflow* generate_scc(models::Dataflow* const dataflow,
+                                 std::map<int, std::vector<ARRAY_INDEX>> scc_map,
+                                 ARRAY_INDEX start_id);
+  models::Dataflow* generate_subgraph(models::Dataflow* const dataflow,
+                                      ARRAY_INDEX lower_limit);
+  void unblock(ARRAY_INDEX id);
+  bool find_simple_cycles_scc(models::Dataflow* const scc,
+                              ARRAY_INDEX start_id, ARRAY_INDEX current_id);
+  std::string print_status(models::Dataflow* const dataflow);
 
 
 }
@@ -27,7 +40,8 @@ void cycle_detection       (models::Dataflow* const  dataflow, parameters_list_t
 
 ADD_TRANSFORMATION(CycleDetection,
 		transformation_t({ "CycleDetection" , "CycleDetection.", algorithms::cycle_detection}));
-
+ADD_TRANSFORMATION(CycleCount,
+                   transformation_t({ "CycleCount" , "Count number of simple cycles in graph.", algorithms::find_simple_cycles}));
 
 
 
