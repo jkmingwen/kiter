91e04c7d02e6e5af72484ceca3ef8a9d9f0973ee
jkmingwen <jkmingwen@gmail.com>
2023-05-05 12:34:40 +0800

    Change select2 select input port type

--- src/libkiter/algorithms/vhdl_generation/reference_files/operators/select2_f250.vhdl
+++ src/libkiter/algorithms/vhdl_generation/reference_files/operators/select2_f250.vhdl
@@ -5,7 +5,7 @@ use ieee.std_logic_unsigned.all;
 
 entity select2_f250 is
     port (clk : in std_logic;
-          X : in  std_logic;
+          X : in  std_logic_vector(8+23+2 downto 0);
           Y : in  std_logic_vector(8+23+2 downto 0);
           Z : in  std_logic_vector(8+23+2 downto 0);
           R : out  std_logic_vector(8+23+2 downto 0)   );
