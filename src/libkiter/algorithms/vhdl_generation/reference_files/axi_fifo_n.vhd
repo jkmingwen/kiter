f41322b3fe572176619982e4db5edd2b5cb67d0d
Bruno <bruno.bodin@yale-nus.edu.sg>
2023-05-05 18:11:51 +0800

    BRAM FIFO

--- src/libkiter/algorithms/vhdl_generation/reference_files/axi_fifo_n.vhd
+++ src/libkiter/algorithms/vhdl_generation/reference_files/axi_fifo_n.vhd
@@ -1,35 +1,8 @@
-----------------------------------------------------------------------------------
--- Company:
--- Engineer:
---
--- Create Date: 07/29/2021 05:08:17 PM
--- Design Name:
--- Module Name: axi_fifo_n - Behavioral
--- Project Name:
--- Target Devices:
--- Tool Versions:
--- Description:
---
--- Dependencies:
---
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
-----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 
 entity axi_fifo_n is
@@ -55,119 +28,93 @@ entity axi_fifo_n is
 end axi_fifo_n;
 
 architecture rtl of axi_fifo_n is
-
-  -- the fifo is full when the ram contains ram_depth elements (extra element for circular buffer)
-  type ram_type is array (0 to ram_depth) of std_logic_vector(buffer_in_data'range);
-  signal ram : ram_type := (others => (others => '0'));
-
-
-  -- newest element at head, oldest element at tail
-  subtype index_type is natural range ram_type'range;
-  signal head : index_type;
-  signal tail : index_type;
-
-  -- local values for outputs
-  signal in_ready_local :  std_logic;
-  signal out_valid_local : std_logic;
-  signal out_data_local :   std_logic_vector(ram_width - 1 downto 0);
-
-  -- increment or wrap the index if this transaction is valid
-  function next_index(
-    index : index_type;
-    ready : std_logic;
-    valid : std_logic) return index_type is
+    -- This is the RAM of the buffer. 
+    type ram_type is array (0 to ram_depth - 1) of std_logic_vector (ram_width - 1 downto 0);
+    signal ram : ram_type;
+    attribute ram_style : string;
+    attribute ram_style of ram : signal is "block";
+    -- index for RAM
+    signal read_index, write_index: natural range 0 to ram_depth - 1 := 0;
+    signal fifo_empty, fifo_full: std_logic := '1';
+function min_value(A_vector: std_logic_vector; B_natural: natural) return natural is
+    variable A_natural : natural;
   begin
-    if ready = '1' and valid = '1' then
-      if index = index_type'high then
-        return index_type'low;
-      else
-        return index + 1;
-      end if;
+    -- Convert A from std_logic_vector to natural
+    A_natural := to_integer(unsigned(A_vector));
+
+    -- Compare A and B and return the smaller
+    if A_natural < B_natural then
+      return A_natural;
+    else
+      return B_natural;
     end if;
-
-    return index;
-  end function;
-
+  end function min_value;
+  
 begin
 
 
-  -- Local values affect outputs
-
-
-
-
-  proc_tail_head_count_inready_outvalid : process(buffer_rst, buffer_clk)
-    variable next_tail : index_type;
-    variable next_head : index_type;
-    variable next_count : index_type;
+  -- FIFO read logic
+  read_logic: process(buffer_clk, buffer_rst)
+  variable tmp_write_index : natural;
+  variable tmp_read_index : natural;
   begin
-
-
-    if buffer_rst = '0' then
-      tail <= index_type'low;
-      head <= index_type'low + ram_init;
-      in_ready_local <= '0';
-      out_valid_local <= '0';
-
-      buffer_in_ready <=   '0';
-      buffer_out_valid <=   '0';
-
-
-      buffer_out_data <= (others => '0');
-      ram <= (others => (others => '0'));
-    else
-
-      if falling_edge(buffer_clk) then
-
-        next_head := next_index(head, in_ready_local, buffer_in_valid);
-        next_tail := next_index(tail, buffer_out_ready, out_valid_local);
-
-        if next_head < next_tail then
-          next_count := next_head - next_tail + ram_depth + 1;
-        else
-          next_count := next_head - next_tail;
-        end if;
-
-
-        -- At this point, next_* is validated, this is going to be the future
-        -- Given what we know from the future, we can make decision
-
-
-        if next_count < ram_depth then
-          in_ready_local <= '1';
-        else
-          in_ready_local <= '0';
-        end if;
-
-        if next_count > 0 then
-          out_valid_local <= '1';
-        else
-          out_valid_local <= '0';
-        end if;
-
-
-        tail <= next_tail;
-        head <= next_head;
-        ram(head) <= buffer_in_data;
-
-        -- We will have a delay in the output if we have tail and head at the same location
-        if head = next_tail then
-          out_data_local <= buffer_in_data;
-        else
-          out_data_local <= ram(next_tail);
-        end if;
+    if buffer_rst = '1' then
+    
+      read_index <= 0;
+      write_index <= ram_init;
+      
+      if ram_init + 1 = ram_depth then
+        fifo_full <= '1';
+      else 
+        fifo_full <= '0';
       end if;
-      if rising_edge(buffer_clk) then
-
-        buffer_in_ready <=  in_ready_local;
-        buffer_out_valid <=  out_valid_local;
-        buffer_out_data <=  out_data_local;
-
+      
+      if ram_init = 0 then
+        fifo_empty <= '1';
+      else 
+        fifo_empty <= '0';
       end if;
+       
+      
+    elsif rising_edge(buffer_clk) then
+    
+      -- Input behavior
+      if buffer_in_valid = '1' and fifo_full = '0' then
+        ram(write_index) <= buffer_in_data;
+        write_index <= (write_index + 1) mod ram_depth;
+        tmp_write_index := (write_index + 1) mod ram_depth;
+        else 
+        tmp_write_index := write_index mod ram_depth;
+      end if;
+      
+      -- Output behavior
+      if buffer_out_ready = '1' and fifo_empty = '0' then
+        buffer_out_data <= ram(read_index);
+        read_index <= (read_index + 1) mod ram_depth;
+        tmp_read_index := (read_index + 1) mod ram_depth;
+        else 
+        tmp_read_index := read_index mod ram_depth;
+      end if;
+      
+      if (tmp_read_index = tmp_write_index) then
+        fifo_empty <= '1' ; 
+       else
+        fifo_empty <= '0';
+      end if;
+      
+      if (((tmp_write_index + 1) mod ram_depth) = tmp_read_index ) then
+        fifo_full <= '1' ; 
+       else
+        fifo_full <= '0';
+      end if;
+      
+   
     end if;
-  end process;
+  end process read_logic;
 
 
-
+  -- Outputs
+  buffer_in_ready <= not fifo_full;
+  buffer_out_valid <= not fifo_empty;
 
 end architecture;
