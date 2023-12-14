85b83dfe3c06c6f2acab320ed0c53d7a8941de7c
jkmingwen <jkmingwen@gmail.com>
2023-12-14 14:39:49 +0800

    Fix bug in FIFO buffer implementation

--- src/libkiter/algorithms/vhdl_generation/reference_files/axi_fifo_n.vhd
+++ src/libkiter/algorithms/vhdl_generation/reference_files/axi_fifo_n.vhd
@@ -1,4 +1,3 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
@@ -28,7 +27,7 @@ entity axi_fifo_n is
 end axi_fifo_n;
 
 architecture rtl of axi_fifo_n is
-    -- This is the RAM of the buffer. 
+    -- This is the RAM of the buffer.
     type ram_type is array (0 to ram_depth - 1) of std_logic_vector (ram_width - 1 downto 0);
     signal ram : ram_type;
     attribute ram_style : string;
@@ -49,7 +48,7 @@ function min_value(A_vector: std_logic_vector; B_natural: natural) return natura
       return B_natural;
     end if;
   end function min_value;
-  
+
 begin
 
 
@@ -59,65 +58,68 @@ begin
   variable tmp_read_index : natural;
   begin
     if buffer_rst = '0' then
-    
+
       read_index <= 0;
       write_index <= ram_init;
-      
+
       if ram_init + 1 = ram_depth then
         fifo_full <= '1';
-      else 
+      else
         fifo_full <= '0';
       end if;
-      
+
       if ram_init = 0 then
         fifo_empty <= '1';
-      else 
+      else
         fifo_empty <= '0';
       end if;
-      
+
       for i in 0 to ram_depth - 1 loop
             ram(i) <= (others => '0');
       end loop;
-      
-    elsif rising_edge(buffer_clk) then
-    
+
+    elsif falling_edge(buffer_clk) then
+
       -- Input behavior
       if buffer_in_valid = '1' and fifo_full = '0' then
         ram(write_index) <= buffer_in_data;
         write_index <= (write_index + 1) mod ram_depth;
         tmp_write_index := (write_index + 1) mod ram_depth;
-        else 
+        else
         tmp_write_index := write_index mod ram_depth;
       end if;
-      
+
       -- Output behavior
       if buffer_out_ready = '1' and fifo_empty = '0' then
-        buffer_out_data <= ram(read_index);
         read_index <= (read_index + 1) mod ram_depth;
         tmp_read_index := (read_index + 1) mod ram_depth;
-        else 
+        else
         tmp_read_index := read_index mod ram_depth;
       end if;
-      
+
       if (tmp_read_index = tmp_write_index) then
-        fifo_empty <= '1' ; 
+        fifo_empty <= '1' ;
        else
         fifo_empty <= '0';
       end if;
-      
+
       if (((tmp_write_index + 1) mod ram_depth) = tmp_read_index ) then
-        fifo_full <= '1' ; 
+        fifo_full <= '1' ;
        else
         fifo_full <= '0';
       end if;
-      
-   
+
+
     end if;
   end process read_logic;
 
-
-  -- Outputs
-  buffer_in_ready <= not fifo_full;
-  buffer_out_valid <= not fifo_empty;
+  write_logic: process(buffer_clk)
+  begin
+    if rising_edge(buffer_clk) then
+      buffer_in_ready <= not fifo_full;
+      buffer_out_valid <= not fifo_empty;
+      buffer_out_data <= ram(read_index);
+    end if;
+  end process write_logic;
 
 end architecture;
