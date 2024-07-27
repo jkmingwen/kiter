c55b682cad5d2d84cef3aeb32b8a2a84077d3b2b
jkmingwen <jkmingwen@gmail.com>
2024-07-28 02:36:02 +0800

    Add commons file for VHDL generation

---
+++ src/libkiter/algorithms/vhdl_generation/VHDLCommons.h
@@ -0,0 +1,12 @@
+/*
+ * VHDLCommons.h
+ *
+ * Created on: 16 June 2024
+ *     Author: jkmingwen
+ */
+#ifndef VHDL_COMMONS_H_
+#define VHDL_COMMONS_H_
+
+enum implType {TT, DD}; // TT: time triggered, DD: data driven
+
+#endif /* VHDL_COMMONS_H_ */
